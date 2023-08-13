98a76abc223f953b36722abe759b3c5258d369bf
pujithavanga <120441900+pujithavanga@users.noreply.github.com>
2023-08-13 21:11:57 +0530

    Add files via upload

---
+++ exp 15.py
@@ -0,0 +1,23 @@
+# Importing required libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# 1. Data Loading
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# 2. Data Splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)  # 70% training, 30% testing
+
+# 3. Model Training & Prediction
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+y_pred = gnb.predict(X_test)
+
+# 4. Evaluation
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
